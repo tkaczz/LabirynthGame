419a98703a453ff5edcc4062c01acd542afe6666
tkaczz <tkaczz@protonmail.com>
2021-02-13 13:42:59 +0100

    Lekcja 2
    Dodany skrypt od poruszania graczem, kamerą
    Zrobiona scena do testowania kontrolera gracza

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [Tooltip("Czułość myszy")]
+    [SerializeField] private float mouseSensivity = 100f;
+
+    private Transform playerBody = null;
+    private Transform cameraTransform = null;
+
+    private float xRotation = 0f;
+
+    //...a w Awake szukający obiektów
+    private void Awake() {
+        playerBody = transform.parent;
+        cameraTransform = GetComponent<Transform>();
+    }
+
+    //dobrym zwyczajem jest żeby w Start() umieszczać kod "konfigurujący"...
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update() {
+        //czego głównie potrzebujemy żeby poruszyć kamerę?
+        //dlaczego * Time.deltaTime?
+        //bez mouseSensivity mouseX, mouseY byłyby bardzo małe, więc do czego ostatecznie by to doprowadziło?
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        //wyliczenie różnicy
+        xRotation -= mouseY;
+
+        //przycięcie maksymalnego kątu
+        xRotation = Mathf.Clamp(xRotation, -90f, 80f);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private float speed = 12f;
+    [SerializeField] private float gravity = -10f;
+    private Vector3 velocity;
+    private float velocity2;
+    private CharacterController characterController = null;
+
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        //musimy skąd pobrac informacje, jak gracz chce sie przesunąć
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        characterController.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+
+        //Vector3 move = (transform.right * x + transform.forward * z) * Time.deltaTime * speed;
+        //move.y = gravity * Time.deltaTime;
+
+        //characterController.Move(move);
+    }
+}
